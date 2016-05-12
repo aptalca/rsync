5a8a0ad1ea56363433a2a205be20a28cdbe39c8c
aptalca <thedjinn@yahoo.com>
2016-05-11 21:13:51 -0400

    Create rsync.sh

---
+++ app/script/rsync.sh
@@ -0,0 +1,6 @@
+echo "***********************************"
+echo "*                                 *"
+echo "*                                 *"
+echo "***********************************"
+echo $date
+echo "Enter your rsync lines here"
